134438943508edbf8d6fab1bad768ad0b735ec77
Darncol <60666804+Darncol@users.noreply.github.com>
2025-01-02 09:23:26 +0200

    Main

---
+++ src/main/java/com/github/darncol/tennismatchscoreboard/Main.java
@@ -0,0 +1,12 @@
+package com.github.darncol.tennismatchscoreboard;
+
+import com.github.darncol.tennismatchscoreboard.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
